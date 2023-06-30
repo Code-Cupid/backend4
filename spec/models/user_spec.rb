5ef1a84e2a5ba80dc3d98289fb1b3c9acd6a63fc
egonz0216 <egonz0216@gmail.com>
2023-06-30 13:59:52 -0700

    completed backend...again

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :model do
-  describe "GET /index" do
-    it 'gets a list of users' do
-      User.create(
-        email: 'test@learn.com', 
-        password: 'password', 
-      )
-      get '/users'
-      user = JSON.parse(response.body)
-      expect(response).to have_http_status(200)
-      expect(user.length).to eq 4
-    end
-  end
+RSpec.describe User, type: :model do
 end
