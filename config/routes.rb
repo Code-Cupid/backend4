35823c67a3db445721cff365c1d9c331cd4ac606
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-06-30 13:33:46 -0400

    Initial setup complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :readmes
   devise_for :users,
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-  # Defines the root path route ("/")
   path: '',
   path_names: {
     sign_in: 'login',
